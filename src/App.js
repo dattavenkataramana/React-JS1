73c5219572e5d4b9a642259e3c836091ca829104
dattavenkataramana <bavirisettidvramana@gmail.com>
2023-01-30 07:35:12 +0000

    datta message

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import UserProfile from './components/UserProfile/index'
+
+const userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'ram',
+  role: 'Software Developer',
+}
+const App = () => <UserProfile userDetails={userDetails} />
+
+export default App
